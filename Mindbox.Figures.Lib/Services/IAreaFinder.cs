3b6386a4ed02da4c7a16b5a07300437ecfaafae1
Рэм Кудусов <rem.kudusov@gmail.com>
2023-09-21 03:06:58 +0300

    Добавлены фигуры и функционал для них.

---
+++ Mindbox.Figures.Lib/Services/IAreaFinder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mindbox.Figures.Lib.Services
+{
+    /// <summary>
+    /// Сервис для нахождения площади фигуры.
+    /// </summary>
+    public interface IAreaFinder
+    {
+        /// <summary>
+        /// Вычислить площадь фигуры.
+        /// </summary>
+        /// <returns>Площадь фигуры.</returns>
+        public double GetArea();
+    }
+}
